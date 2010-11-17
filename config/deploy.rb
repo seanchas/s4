3b5ae4561c4a1b72207e769decd6ac832d28f17d
seanchas <seanchas@gmail.com>
2010-11-17 11:00:34 +0300

    capified

---
+++ config/deploy.rb
@@ -0,0 +1,43 @@
+set :application, "cabinet"
+set :repository,  "http://github.com/seanchas/s4.git"
+set :user,        :ror
+set :runner,      :ror
+set :use_sudo,    false
+
+set :deploy_to,   "/export/depo/ror/linux/appservers/#{application}"
+set :deploy_via,  :copy
+
+set :scm, :git
+
+role :web, "dls8"
+role :app, "dls8"
+role :db,  "dls8", :primary => true
+
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
+
+configuration = [
+  'config/database.yml',
+  'config/initializers/s4.rb',
+  'config/initializers/cookie_verification_secret.rb',
+  'vendor/rails'
+]
+
+namespace :deploy do
+
+  task :update_configuration, :roles => :app do
+    configuration.each do |entry|
+      run <<-CMD
+        if [ -f #{release_path}/#{entry} ]; then rm -f #{release_path}/#{entry}; fi; ln -s #{shared_path}/rails/#{entry} #{release_path}/#{entry}
+      CMD
+    end
+  end
+  
+end
+
+after "deploy:update_code", "deploy:update_configuration"
