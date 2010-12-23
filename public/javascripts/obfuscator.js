eaaff51350e76185f0b2a488aaf79b881132fad3
seanchas <seanchas@gmail.com>
2010-12-23 12:19:42 +0300

    added rot13 to obfuscator

--- public/javascripts/obfuscator.js
+++ public/javascripts/obfuscator.js
@@ -1,3 +1,9 @@
+String.prototype.rot13 = function() {
+  return this.replace(/[a-zA-Z]/g, function(c) {
+    return String.fromCharCode((c <= 'Z' ? 90 : 122) >= (c = c.charCodeAt(0) + 13) ? c : c - 26);
+  });
+}
+
 var Deobfuscator = {
   
   run: function() {
@@ -7,7 +13,7 @@ var Deobfuscator = {
   restore: function(node) {
     switch(node.readAttribute("data-obfuscate")) {
       case 'email':
-        node.update(node.innerHTML.gsub('+', '@').gsub('/', '.')); break
+        node.update(node.innerHTML.gsub(',', '@').gsub('/', '.').rot13()); break
     }
     node.removeAttribute('data-obfuscate');
   }
