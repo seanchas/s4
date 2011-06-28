c4b217be8d8f601e56c9ceb14aeae10adee41a54
seanchas <seanchas@gmail.com>
2011-06-28 17:34:58 +0400

    failure/success messages

--- app/controllers/insiders_controller.rb
+++ app/controllers/insiders_controller.rb
@@ -5,9 +5,10 @@ class InsidersController < ApplicationController
   before_filter :check_authentication
   
   def create
-    @error = flash[:recaptcha_error] and render :new and return unless verify_recaptcha
-    @error = ".errors.file_missing" and render :new and return unless params[:file].present?
+    flash.now[:error] = flash[:recaptcha_error] and render :new and return unless verify_recaptcha
+    flash.now[:error] = ".errors.file_missing" and render :new and return unless params[:file].present?
     InsidersMailer.deliver_insider_file(authenticated_user.email, params[:file])
+    flash[:notice] = ".success"
     redirect_to insider_path
   end
 
