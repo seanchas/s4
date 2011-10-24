ea11e9e090e57044844302fdc345b58823c71471
seanchas <seanchas@gmail.com>
2011-10-24 18:00:56 +0400

    added htmldoc gem

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ gem "mysql"
 gem "nokogiri"
 gem "RedCloth"
 gem "warden"
+gem "htmldoc"
 
 gem "recaptcha", :require => "recaptcha/rails"
 
--- app/models/authority.rb
+++ app/models/authority.rb
@@ -1,6 +1,6 @@
 class Authority < Base
   def self.getAuthorityTypes
-    #S4.site = 'http://s4-beta.micex.ru/S4XmlRpcService'
+    S4.site = 'http://s4-beta.micex.ru/S4XmlRpcService'
     @sessionId = S4.connection.call("s4.openSession", I18n.locale)
     @typeId = S4.connection.call("s4.getResource", @sessionId, 'warrant_type', "76831d27-6daf-44af-bb73-a72875e9a04f");
     @typeId = S4::Resource.parse_many(@typeId)
@@ -13,7 +13,7 @@ class Authority < Base
   end
   
   def self.getOrganisationUsers
-    #S4.site = 'http://s4-beta.micex.ru/S4XmlRpcService'
+    S4.site = 'http://s4-beta.micex.ru/S4XmlRpcService'
     @sessionId = S4.connection.call("s4.openSession", I18n.locale)
     @typeId = S4.connection.call("s4.getResource", @sessionId, 'new_warrant_agent', "76831d27-6daf-44af-bb73-a72875e9a04f");
     @typeId = S4::Resource.parse_many(@typeId)
