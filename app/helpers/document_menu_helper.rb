f0b38767115fb09dadbc6b680cacf3f385e250dc
Roman A. Makarov <roman@roman-laptop-job.site-job>
2011-10-28 17:09:53 +0400

    Exclude send message

--- app/helpers/document_menu_helper.rb
+++ app/helpers/document_menu_helper.rb
@@ -5,7 +5,7 @@ module DocumentMenuHelper
       ul.li t(:header, :scope => [:shared, :senddocument]), list_senddocuments_path,             :senddocuments => :list
       #ul.li "Регистрационные карточки", licenses_organization_path,    :organizations => :licenses
       ul.li t(:header, :scope => [:shared, :authority]), authorities_path,  :authorities
-      ul.li t(:header_message, :scope => [:shared, :senddocument]), message_senddocuments_path,     :senddocuments => :message
+      #ul.li t(:header_message, :scope => [:shared, :senddocument]), message_senddocuments_path,     :senddocuments => :message
       ul.li t(:header_document, :scope => [:shared, :senddocument]), form_senddocuments_path,     :senddocuments => :form
     end
   end
