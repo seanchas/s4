56e0e23995c73239c55419a194a964827d1d2a87
zingorn <zingorn@gmail.com>
2012-02-06 14:02:33 +0400

    fix columns size in reg_card_errors table

--- db/migrate/20120202120857_create_reg_card_errors.rb
+++ db/migrate/20120202120857_create_reg_card_errors.rb
@@ -1,18 +1,18 @@
 class CreateRegCardErrors < ActiveRecord::Migration
   def self.up
     create_table :reg_card_errors do |t|
-      t.text  :common_errors, :limit => 4294967295
-      t.text  :show, :limit => 4294967295
-      t.text  :licenses, :limit => 4294967295
-      t.text  :ceo, :limit => 4294967295
-      t.text  :controllers, :limit => 4294967295
-      t.text  :structure, :limit => 4294967295
-      t.text  :capital, :limit => 4294967295
-      t.text  :filials, :limit => 4294967295
-      t.text  :ncc_federal_law, :limit => 4294967295
-      t.text  :phones, :limit => 4294967295
-      t.text  :sendcard, :limit => 4294967295
-      t.text  :contactlist, :limit => 4294967295
+      t.text  :common_errors, :limit => 16777215
+      t.text  :show, :limit => 16777215
+      t.text  :licenses, :limit => 16777215
+      t.text  :ceo, :limit => 16777215
+      t.text  :controllers, :limit => 16777215
+      t.text  :structure, :limit => 16777215
+      t.text  :capital, :limit => 16777215
+      t.text  :filials, :limit => 16777215
+      t.text  :ncc_federal_law, :limit => 16777215
+      t.text  :phones, :limit => 16777215
+      t.text  :sendcard, :limit => 16777215
+      t.text  :contactlist, :limit => 16777215
       t.string :user
     end
   end
