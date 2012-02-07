cb9970862900fe2437de49d5d143ee03beaffee1
zingorn <zingorn@gmail.com>
2012-02-07 13:38:58 +0400

    fix of comment element

--- vendor/plugins/formtastic-ext/lib/formtastic-ext/comment.rb
+++ vendor/plugins/formtastic-ext/lib/formtastic-ext/comment.rb
@@ -1,15 +1,18 @@
 module Formtastic #:nodoc:
   class SemanticFormBuilder
     def comment_input(method, options)
-      opts ||= {}
+      opts = {}
       opts = options[:i18n] if !options[:i18n].nil?
+      opts[:scope] = {}
+
+      template.logger.debug "#{opts.to_yaml}\n\n#{options.to_yaml}"
       comment = ::Formtastic::I18n.t("comments.#{options[:labelPath]}.#{method}", opts)
 
       wrapper_html = options.delete(:wrapper_html) || {}
       wrapper_html[:id]  ||= generate_html_id(method)
       wrapper_html[:class] = ([] << wrapper_html[:class]).flatten.compact.join(' ')
-      #return template.content_tag(:li, Formtastic::Util.html_safe(comment), wrapper_html)
-      comment
+
+      Formtastic::Util.html_safe(comment)#comment
     end
   end
 end
