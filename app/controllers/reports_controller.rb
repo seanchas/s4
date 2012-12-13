c5d77d8462e353dd8dd7b4409c7c22b9a16efb4d
zingorn <zingorn@gmail.com>
2012-12-13 14:25:08 +0400

    reports menu selection fixed

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,5 +1,9 @@
 class ReportsController < ApplicationController
-  
+
+  helper :members_menu
+  include OrganizationsHelper
+  include ApplicationHelper
+
   def index
     @organization     = S4::Organization.find(s4_user)
     #@weekly_reports   = Reports::WeeklyReport.all(s4_user)
