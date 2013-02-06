9ce9c153dae960395d3365c02bb49878ec5fc924
koiiiey <pavel.makhnev@bk.ru>
2013-02-06 14:20:15 +0300

    hotfix for /cards/sendcard fixed error due to database change

--- app/models/s4/filial_info.rb
+++ app/models/s4/filial_info.rb
@@ -5,7 +5,7 @@ module S4
     self.resource_type = :filial_info
     def self.create_xml(row)
       if !row.nil? && row.no_moscow == true
-        row.moscow_addr = row.moscow_tel_areacode = row.moscow_tel = row.moscow_boss = row.moscow_boss_firstname = nil
+        row.moscow_addr = row.moscow_tel_areacode = row.moscow_tel_display = row.moscow_boss = row.moscow_boss_firstname = nil
         row.moscow_boss_patronymic = row.moscow_boss_position = nil
       end
       S4::Card.create_common_single_xml('filial_info', row, ["user"], true)
